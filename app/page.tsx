ccc8893d8fbe4896dc91594c5e9e39fba979c127
Shivam <shivamnauriyal1224@gmail.com>
2025-04-29 21:35:40 +0530

    experiences

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Experience from "@/components/Experience/Experience";
 import HeroSection from "@/components/HeroSection/HeroSection";
 
 export default function Home() {
     return (
         <>
             <HeroSection />
+            <Experience />
         </>
     );
 }
--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -6,99 +6,105 @@ import { HeroParallax } from "../ui/backgrounds/hero-parallax";
 export default function HeroSection() {
     const products = [
         {
-            title: "Moonbeam",
-            link: "https://gomoonbeam.com",
+            title: "Coca-Cola",
+            link: "https://www.coca-cola.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+                "https://www.coca-colacompany.com/content/dam/journey/us/en/brands/coca-cola/coca-cola-logo.png", // Official Coca-Cola PNG
         },
         {
-            title: "Cursor",
-            link: "https://cursor.so",
+            title: "Sprite",
+            link: "https://www.sprite.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+                "https://www.coca-colacompany.com/content/dam/journey/us/en/brands/coca-cola/coca-cola-logo.png", // Official Coca-Cola PNG
+            // "https://www.coca-colacompany.com/content/dam/journey/us/en/brands/sprite/sprite-logo.png", // Official Sprite PNG
         },
         {
-            title: "Rogue",
-            link: "https://userogue.com",
+            title: "McDonald's",
+            link: "https://www.mcdonalds.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-        },
-
-        {
-            title: "Editorially",
-            link: "https://editorially.org",
-            thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+                "https://www.coca-colacompany.com/content/dam/journey/us/en/brands/coca-cola/coca-cola-logo.png", // Official Coca-Cola PNG
+            // "https://www.mcdonalds.com/content/dam/sites/usa/nfl/public/brand-logos/mcdonalds-logo.png", // Official McDonald's PNG
         },
         {
-            title: "Editrix AI",
-            link: "https://editrix.ai",
+            title: "Ferrari",
+            link: "https://www.ferrari.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+                "https://www.ferrari.com/sites/en/en-en/logo/ferrari-logo-black.png", // Official Ferrari PNG
         },
         {
-            title: "Pixel Perfect",
-            link: "https://app.pixelperfect.quest",
+            title: "Harper's Bazaar India",
+            link: "https://www.harpersbazaar.in/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-        },
-
-        {
-            title: "Algochurn",
-            link: "https://algochurn.com",
-            thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Harper%27s_Bazaar_Logo.svg/1200px-Harper%27s_Bazaar_Logo.svg.png", // Wikimedia PNG
         },
         {
-            title: "Aceternity UI",
-            link: "https://ui.aceternity.com",
+            title: "Delhi Times",
+            link: "https://timesofindia.indiatimes.com/delhi-times",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Times_of_India_logo.svg/1200px-Times_of_India_logo.svg.png", // Wikimedia PNG
         },
         {
-            title: "Tailwind Master Kit",
-            link: "https://tailwindmasterkit.com",
+            title: "Royal Enfield",
+            link: "https://www.royalenfield.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+                "https://www.royalenfield.com/content/dam/royalenfield/india/home-page/logo/re-logo.png", // Official Royal Enfield PNG
         },
         {
-            title: "SmartBridge",
-            link: "https://smartbridgetech.com",
+            title: "Nike",
+            link: "https://www.nike.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/1200px-Logo_NIKE.svg.png", // Wikimedia PNG
         },
         {
-            title: "Renderwork Studio",
-            link: "https://renderwork.studio",
+            title: "Adidas",
+            link: "https://www.adidas.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-        },
-
-        {
-            title: "Creme Digital",
-            link: "https://cremedigital.com",
-            thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1200px-Adidas_Logo.svg.png", // Wikimedia PNG
         },
         {
-            title: "Golden Bells Academy",
-            link: "https://goldenbellsacademy.com",
+            title: "Apple",
+            link: "https://www.apple.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1200px-Apple_logo_black.svg.png", // Wikimedia PNG
         },
         {
-            title: "Invoker Labs",
-            link: "https://invoker.lol",
+            title: "Google",
+            link: "https://www.google.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png", // Wikimedia PNG
         },
         {
-            title: "E Free Invoice",
-            link: "https://efreeinvoice.com",
+            title: "Amazon",
+            link: "https://www.amazon.com/",
             thumbnail:
-                "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png", // Wikimedia PNG
+        },
+        {
+            title: "Samsung",
+            link: "https://www.samsung.com/",
+            thumbnail:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/1200px-Samsung_Logo.svg.png", // Wikimedia PNG
+        },
+        {
+            title: "Tesla",
+            link: "https://www.tesla.com/",
+            thumbnail:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1200px-Tesla_Motors.svg.png", // Wikimedia PNG
+        },
+        {
+            title: "Netflix",
+            link: "https://www.netflix.com/",
+            thumbnail:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png", // Wikimedia PNG
+        },
+        {
+            title: "Microsoft",
+            link: "https://www.microsoft.com/",
+            thumbnail:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1200px-Microsoft_logo.svg.png", // Wikimedia PNG
         },
     ];
+
     return (
         // <section className="relative w-full h-screen overflow-hidden grad-fade-right bg-gradient-to-br text-white">
         //     <Boxes className="mask-fade-right" />
