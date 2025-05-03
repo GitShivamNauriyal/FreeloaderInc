14157ca3874ea22985c15eda9f4d4b199e865b32
Shivam <shivamnauriyal1224@gmail.com>
2025-05-03 14:51:40 +0530

    progress

---
+++ app/experiences/MidHeading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaArrowDown } from "react-icons/fa";
+
+const MidHeading = () => {
+    return (
+        <div className="px-6 py-12 md:p-16 lg:pt-24 pb-12 bg-white">
+            <h1 className="px-4 md:px-20 lg:px-56 max-w-screen-xl mx-auto text-lg sm:text-xl lg:text-4xl text-center text-[#5e17eb] font-bold leading-5 lg:leading-8 tracking-wide uppercase">
+                95% of marketers agree that live events provide attendees with a
+                valuable opportunity to form in-person connections in an
+                increasingly digital world
+            </h1>
+
+            {/* Arrow and "Showreel" text */}
+            <div className="mt-4 lg:mt-6 flex flex-col items-center">
+                <span className="text-cyan-500 font-medium">Showreel</span>
+                <FaArrowDown className="text-cyan-500 text-xl mb-1" />
+            </div>
+        </div>
+    );
+};
+
+export default MidHeading;
--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BrandExperiences from "./BrandExp";
+import MidHeading from "./MidHeading";
 
 const page = () => {
     return (
         <div className="mt-4">
             <BrandExperiences />
+            <MidHeading />
         </div>
     );
 };
