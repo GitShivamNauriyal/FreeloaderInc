b07c044353f4c5f9ca29110ca5cff61357fdbde5
Shivam <shivamnauriyal1224@gmail.com>
2025-05-03 15:44:32 +0530

    mid heading

--- app/influencers/page.tsx
+++ app/influencers/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TopInfluencers from "./TopInf";
+import MidHeading from "./MidHeading";
 
 const page = () => {
     return (
         <div>
             <TopInfluencers />
+            <MidHeading />
         </div>
     );
 };
